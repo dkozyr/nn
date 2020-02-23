aa0cd306cc5e47dd628d2ef8dfae802ade493945
Denys Kozyr <dkozyr@gmail.com>
2020-02-23 23:02:16 +0200

    disabled test

--- nn-test/Conv3DTest.cpp
+++ nn-test/Conv3DTest.cpp
@@ -27,7 +27,7 @@ protected:
     }
 };
 
-TEST_F(Conv3DTest, DISBLED_Forward) {
+TEST_F(Conv3DTest, DISABLED_Forward) {
     const auto N = 16;
     const auto conv_size = 2;
     const auto rgb_layers = 3;
@@ -46,29 +46,6 @@ TEST_F(Conv3DTest, DISBLED_Forward) {
     ASSERT_EQ(Y_cpu.GetShape().cols, out_neurons);
     ASSERT_EQ(Y_cuda.GetShape().rows, N);
     ASSERT_EQ(Y_cuda.GetShape().cols, out_neurons);
-}
-
-TEST_F(Conv3DTest, DISABLED_Forward) {
-    for(auto begin = Clock::now(); Clock::now() - begin < 200ms; ) {
-        const auto N = Rand<size_t>(1, 64);
-        const auto conv_size = Rand<size_t>(2, 5);
-        const auto rgb_layers = 3;
-        const Shape input{rgb_layers, Rand<size_t>(conv_size, 64), Rand<size_t>(conv_size, 64)};
-
-        Matrix<Cpu> X_cpu(Shape{N, input.Size()}, Rand(-3.0f, 3.0f));
-        Matrix<Cuda> X_cuda(Shape{N, input.Size()});
-        X_cuda.CopyHostData(X_cpu);
-        X_cuda.CopyHostToDevice();
-
-        const auto& Y_cpu = TestForward<Cpu>(X_cpu, input, conv_size);
-        const auto& Y_cuda = TestForward<Cuda>(X_cuda, input, conv_size);
-
-        const auto out_neurons = /*input.layers **/ (input.rows - conv_size + 1) * (input.cols - conv_size + 1);
-        ASSERT_EQ(Y_cpu.GetShape().rows, N);
-        ASSERT_EQ(Y_cpu.GetShape().cols, out_neurons);
-        ASSERT_EQ(Y_cuda.GetShape().rows, N);
-        ASSERT_EQ(Y_cuda.GetShape().cols, out_neurons);
-    }
 }
 
 } //namespace layer
